510e7456644c79b946ba238f3cd2738da4dde88c
mcruz <mcruzpr86@gmail.com>
2020-04-21 22:35:01 -0400

    main page established before editing

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//Global Variables
+//--------------------------------------------------------------------
+//Arrays and Variables for holding data
+
+
+//Game counters
+let winCount = 0;
+let lossCount = 0;
+let guessesLeft = 0;
+ 
+//Functions (Reusable blocks of code)
+//--------------------------------------------------------------------
+
+//Main Process
+//--------------------------------------------------------------------
